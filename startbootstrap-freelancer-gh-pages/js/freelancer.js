cf259ac0446bf59a2edc7b791c5f3b83670ec04c
Valtteri Kinnunen <walterskinn@gmail.com>
2017-12-20 06:08:38 +0200

    hg

--- startbootstrap-freelancer-gh-pages/js/freelancer.js
+++ startbootstrap-freelancer-gh-pages/js/freelancer.js
@@ -50,11 +50,7 @@
   $(window).scroll(navbarCollapse);
 
   // Modal popup$(function () {
-  $('.portfolio-item').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true
+
   });
   $(document).on('click', '.portfolio-modal-dismiss', function(e) {
     e.preventDefault();
